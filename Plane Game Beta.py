c22d8b3dc8cd09f9b6b5f0f919fd0bbbfc95c972
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2019-05-23 12:43:03 -0400

    Add files via upload

--- Plane Game Beta.py
+++ Plane Game Beta.py
@@ -2,12 +2,14 @@ import arcade
 
 
 WIDTH = 1600
-HEIGHT = 1200
+HEIGHT = 960
 
 # Loading Textures
 # title = arcade.load_texture('title.png', 0, 0, 800, 268)
 background = arcade.load_texture('title screen background.jpg', 0, 0, 1600, 1200)
-
+x_background = WIDTH / 2
+scroll_left = True
+scroll_right = False
 
 def setup():
     arcade.open_window(WIDTH, HEIGHT, "Plane Game Pre-pree-reee")
@@ -30,7 +32,8 @@ def update(delta_time):
 
 def on_draw():
     arcade.start_render()
-    background()
+
+    draw_background()
 
 
 def on_key_press(key, modifiers):
@@ -45,10 +48,25 @@ def on_mouse_press(x, y, button, modifiers):
     pass
 
 
-def background():
-    x_background = WIDTH / 2
+def draw_background():
+    global x_background
+    global scroll_left
+    global scroll_right
+
     arcade.draw_texture_rectangle(x_background, HEIGHT / 2, WIDTH, HEIGHT, background)
     arcade.draw_texture_rectangle(x_background + WIDTH, HEIGHT / 2, WIDTH, HEIGHT, background)
+    arcade.draw_texture_rectangle(x_background - WIDTH, HEIGHT / 2, WIDTH, HEIGHT, background)
+    if x_background == -1600:
+        scroll_right = True
+        scroll_left = False
+    elif x_background == 3200:
+        scroll_left = True
+        scroll_right = False
+    if scroll_right:
+        x_background += 0.5
+    elif scroll_left:
+        x_background -= 0.5
+
 
 
 if __name__ == '__main__':
